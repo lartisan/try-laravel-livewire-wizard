f4786b3dd7ee6874b47ad80a0106771a4d8f5447
Cristian Iosif <cristianiosif@me.com>
2025-09-03 14:05:08 +0300

    Add offer

--- app/Livewire/CheckoutWizardComponent.php
+++ app/Livewire/CheckoutWizardComponent.php
@@ -46,7 +46,7 @@ class CheckoutWizardComponent extends WizardComponent
 
     private function getOffer()
     {
-        $fileContent = file_get_contents("/Users/cristianiosif/Downloads/oferta.json");
+        $fileContent = file_get_contents(resource_path('assets/oferta.json'));
 
         return json_decode($fileContent, true);
     }
